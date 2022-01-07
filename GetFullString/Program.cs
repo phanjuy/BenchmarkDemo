742240e4a1e5866c901f95c28544b6650166f061
Duy Phan <dpha@netcompany.com>
2022-01-07 11:59:32 +0100

    Add GetFullString

---
+++ GetFullString/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace GetFullString
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Demo>();
+        }
+    }
+
+
+    [MemoryDiagnoser]
+    public class Demo
+    {
+        [Benchmark]
+        public string GetFullStringNormally()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < 100; i++)
+            {
+                output += i;
+            }
+            return output;
+        }
+
+        [Benchmark]
+        public string GetFullStringWithStringBuilder()
+        {
+            var output = new StringBuilder();
+            for (int i = 0; i < 100; i++)
+            {
+                output.Append(i);
+            }
+            return output.ToString();
+        }
+    }
+}
---
+++ GetFullString/README.md
@@ -0,0 +1,6 @@
+# Guide
+
+The best way is build benchmark in the Release mode and run it from the command line.
+
+- `dotnet build -c Release`
+- `dotnet .\bin\Release\net6.0\GetFullString.dll`
