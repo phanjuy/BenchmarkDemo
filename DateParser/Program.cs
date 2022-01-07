edb5be50647ce0249dc4c175e859e071f11bbfa3
Duy Phan <dpha@netcompany.com>
2022-01-07 11:14:40 +0100

    Add DateParser

---
+++ DateParser/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace DateParser;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<DateParserBenchmarks>();
+    }
+}
